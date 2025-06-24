b18dbd55dad1fc36f28cf8138c60b7e8d197318c
muhammed Razi pp <razipp5850@gmail.com>
2025-06-24 12:46:38 +0530

    updated

--- project/project/settings.py
+++ project/project/settings.py
@@ -91,7 +91,7 @@ DATABASES = {
         'USER': config('DB_USER'),
         'PASSWORD': config('DB_PASSWORD'),
         'HOST': config('DB_HOST'),
-        'PORT': config('DB_PORT'),
+        'PORT': 5432
     }
 }
 
